a5ef38d75f83891769693e2032ee8c4cd3e276f7
Raindeezy <roviir@gmail.com>
2015-12-16 13:12:54 -0800

    removed redux-logger

--- src/common/store/configureStore.js
+++ src/common/store/configureStore.js
@@ -2,7 +2,6 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import { reduxReactRouter } from 'redux-router';
 import thunk from 'redux-thunk';
 import createHistory from 'history/lib/createBrowserHistory';
-import createLogger from 'redux-logger';
 import promiseMiddleware from '../api/promiseMiddleware';
 import rootReducer from '../reducers';
 
@@ -22,7 +21,7 @@ const middlewareBuilder = () => {
         })
       ]
     }else{
-      middleware = applyMiddleware(...universalMiddleware,createLogger());
+      middleware = applyMiddleware(...universalMiddleware);
       allComposeElements = [
         middleware,
         reduxReactRouter({
